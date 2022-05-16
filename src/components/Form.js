c969acb29a241c6d2774ffbe4e40565be552dbe2
Njoki <kinuthiash@gmail.com>
2022-05-16 11:26:58 +0300

    completed assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,43 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+function Form(
+  { firstName,
+    lastName,
+    handleFirstNameChange,
+    handleLastNameChange
+  }) {
 
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" onChange={handleFirstNameChange} value={firstName} />
+      <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
   );
 }
+
+// function Form() {
+//   const [newsletter, setNewsletter] = useState(false);
+
+//   function handleNewsletterChange(event) {
+//     // .checked, not .value!
+//     setNewsletter(event.target.checked);
+//   }
+
+//   return (
+//     <form>
+//       <label htmlFor="newsletter">Subscribe to our Newsletter?</label>
+//       <input
+//         type="checkbox"
+//         id="newsletter"
+//         onChange={handleNewsletterChange}
+//         // {/* checked instead of value */}
+//         checked={newsletter}
+//       />
+//       <button type="submit">Submit</button>
+//     </form>
+//   );
+// }
+
 
 export default Form;
